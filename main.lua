ae879faf132712fd77c03c4b316e7744dd408dd7
William Ambrose <W.P.Ambrose@gmail.com>
2019-01-09 02:29:37 -0500

    Set things up for data handling; tweaked structuring and input some more

--- main.lua
+++ main.lua
@@ -1,20 +1,32 @@
 game_status = {
 	action = "pause",
-	menu = nil
+	timer = 0
 }
 
 game_board = {}
 
+graphics = {
+	air = nil,
+	fire = nil,
+	earth = nil,
+	water = nil
+}
+
+
 function love.keypressed(key)
-	if key == "escape" then
-		if game_status.action == "pause" then
+	if game_status.action == "pause" then
+		if key == "escape" then
 			game_status.action = "play"
-		elseif game_status.action == "play" then
+		end
+	elseif game_status.action == "play" then
+		if key == "escape" then
 			game_status.action = "pause"
 		end
-	elseif key == "return" then
-		if game_status.action == "quit" then
+	elseif game_status.action == "quit" then
+		if key == "return" or key == "y" then
 			love.event.quit()
+		elseif key == "escape" or key == "n" then
+			game_status.action = "pause"
 		end
 	end
 end
@@ -31,23 +43,43 @@ function love.quit()
 	end
 end
 
+
 function love.load()
 	love.graphics.setBackgroundColor(16, 16, 16)
 	
-	air = love.graphics.newImage("assets/air.png")
-	fire = love.graphics.newImage("assets/fire.png")
-	earth = love.graphics.newImage("assets/earth.png")
-	water = love.graphics.newImage("assets/water.png")
+	graphics.air = love.graphics.newImage("assets/air.png")
+	graphics.fire = love.graphics.newImage("assets/fire.png")
+	graphics.earth = love.graphics.newImage("assets/earth.png")
+	graphics.water = love.graphics.newImage("assets/water.png")
 	
 	for row = 1, love.graphics.getWidth() / 16 do
 		game_board[row] = {}
 		for column = 1, love.graphics.getHeight() / 16 do
-			if row == 1 then
-				game_board[row][column] = "fire"
-			elseif row == 2 then
-				game_board[row][column] = "water"
-			else
-				game_board[row][column] = "empty"
+			game_board[row][column] = {
+				element = "none",
+				aspects = {
+					hot = 0,
+					dry = 0,
+					cold = 0,
+					wet = 0
+				}
+			}
+			if row == 2 then
+				game_board[row][column].element = "fire"
+				game_board[row][column].aspects.hot = 4
+				game_board[row][column].aspects.dry = 4
+			elseif row == 4 then
+				game_board[row][column].element = "earth"
+				game_board[row][column].aspects.dry = 4
+				game_board[row][column].aspects.cold = 4
+			elseif row == 6 then
+				game_board[row][column].element = "water"
+				game_board[row][column].aspects.cold = 4
+				game_board[row][column].aspects.wet = 4
+			elseif row == 8 then
+				game_board[row][column].element = "air"
+				game_board[row][column].aspects.wet = 4
+				game_board[row][column].aspects.hot = 4
 			end
 		end
 	end
@@ -57,59 +89,60 @@ end
 function love.update(dt)
 	if game_status == "play" then
 		-- affect the world
+		if timer >= 0 and timer < 1 then
+			timer = timer + dt
+		elseif timer >= 1 then
+			-- the cycle is complete
+			for row_index, row_contents in pairs(game_board) do
+				for column_index, tile_contents in pairs(game_board[row_index]) do
+					-- iterate over cells
+					
+				end
+			end
+			timer = 0
+		end
 	end
 end
 
 function love.draw()
 	local top = 0
 	local side = 0
-	local screen = {}
 	
-	love.graphics.setColor(224, 224, 224, 255)
+	if game_status.action == "play" then
+		love.graphics.setColor(255, 255, 255, 255)
+	elseif game_status.action == "pause" or game_status.action == "quit" then
+		love.graphics.setColor(128, 128, 128, 255)
+	end
+	
 	love.graphics.setLineWidth(2)
-	
 	for side = 0, 320, 16 do
 		love.graphics.line(0, side, 480, side)
 	end
-	
 	for top = 0, 480, 16 do
 		love.graphics.line(top, 0, top, 320)
 	end
 	
-	love.graphics.setColor(255, 255, 255, 255)
-	
-	
-	if game_status.action == "play" then
-		screen = {255, 255, 255, 255}
-	elseif game_status.action == "pause" or game_status.action == "quit" then
-		screen = {128, 128, 128, 255}
-	end
-	
 	for row_index, row_contents in pairs(game_board) do
-		for column_index, tile_contents in pairs(game_board[row_index]) do
-			if tile_contents == "empty" then
-				love.graphics.setColor(16, 16, 16, 255)
-				love.graphics.rectangle("fill", (row_index * 16 - 16) + 1, (column_index * 16 - 16) + 1, 14, 14)
-				love.graphics.setColor(255, 255, 255, 255)
-			elseif tile_contents == "air" then
-				love.graphics.setColor(screen)
-				love.graphics.draw(air, (row_index * 16 - 16), (column_index * 16 - 16))
-			elseif tile_contents == "fire" then
-				love.graphics.setColor(screen)
-				love.graphics.draw(fire, (row_index * 16 - 16), (column_index * 16 - 16))
-			elseif tile_contents == "earth" then
-				love.graphics.setColor(screen)
-				love.graphics.draw(earth, (row_index * 16 - 16), (column_index * 16 - 16))
-			elseif tile_contents == "water" then
-				love.graphics.setColor(screen)
-				love.graphics.draw(water, (row_index * 16 - 16), (column_index * 16 - 16))
+		for column_index, cell_contents in pairs(game_board[row_index]) do
+			if cell_contents.element == "none" then
+				-- leave the cell blank
+			elseif cell_contents.element == "air" then
+				love.graphics.draw(graphics.air, (row_index * 16 - 16), (column_index * 16 - 16))
+			elseif cell_contents.element == "fire" then
+				love.graphics.draw(graphics.fire, (row_index * 16 - 16), (column_index * 16 - 16))
+			elseif cell_contents.element == "earth" then
+				love.graphics.draw(graphics.earth, (row_index * 16 - 16), (column_index * 16 - 16))
+			elseif cell_contents.element == "water" then
+				love.graphics.draw(graphics.water, (row_index * 16 - 16), (column_index * 16 - 16))
 			end
 		end
 	end
 	
+	love.graphics.setColor(255, 255, 255, 255)
+	
 	if game_status.action == "pause" then
-		love.graphics.printf( { {255, 255, 255, 255}, "S I M U L A T I O N   P A U S E D" }, 0, 112, (love.graphics.getWidth()), "center")
+		love.graphics.printf( { {240, 240, 240, 255}, "SIMULATION PAUSED" }, 0, 112, (love.graphics.getWidth()), "center")
 	elseif game_status.action == "quit" then
-		love.graphics.printf( { {255, 255, 255, 255}, "QUIT GAME?" }, 0, 112, (love.graphics.getWidth()), "center")
+		love.graphics.printf( { {240, 240, 240, 255}, "QUIT SIMULATION? (Y/N)" }, 0, 112, (love.graphics.getWidth()), "center")
 	end
 end
