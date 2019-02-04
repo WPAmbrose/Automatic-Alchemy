a8fa8156cd7345bdc6bc90ce70f43a75f72db466
William Ambrose <W.P.Ambrose@gmail.com>
2019-02-04 01:24:14 -0500

    Added a readme; disabled unusued modules

--- conf.lua
+++ conf.lua
@@ -3,5 +3,12 @@ function love.conf(t)
 	t.version = "0.10.2"
 	t.window.width = 480
 	t.window.height = 320
-	t.console = true
+	t.console = false
+	t.modules.physics = false
+	t.modules.mouse = false
+	t.modules.joystick = false
+	t.modules.sound = false
+	t.modules.video = false
+	t.modules.thread = false
+	t.modules.system = false
 end
